1b61883ecdc2128e07c2a8f687f8d4130d8280cb
jmaxwellwestga <jm00724@my.westga.edu>
2024-03-10 22:36:43 -0400

    added remaining fields to in memory model

--- Model/MemberX.cs
+++ Model/MemberX.cs
@@ -10,7 +10,15 @@ namespace RentMeApp.Model
         public int MemberID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public char Sex { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State {  get; set; }
+        public string Zip { get; set; }
         public string Phone { get; set; }
+
        
 
         /// <summary>
--- View/AddMemberDialog.Designer.cs
+++ View/AddMemberDialog.Designer.cs
@@ -444,7 +444,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(868, 451);
+            this.ClientSize = new System.Drawing.Size(868, 460);
             this.Controls.Add(this.tableLayoutPanel3);
             this.Controls.Add(this.AddMemberBtn);
             this.Controls.Add(this.tableLayoutPanel2);
