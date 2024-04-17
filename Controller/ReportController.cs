81b8c0f0a789213cf1ac0d41b26d773f08d18bcc
jmaxwellwestga <jm00724@my.westga.edu>
2024-04-17 19:46:38 -0400

    report controller created

---
+++ Controller/ReportController.cs
@@ -0,0 +1,20 @@
+﻿using RentMeApp.DAL;
+using RentMeApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentMeApp.Controller
+{
+    public class ReportController
+    {
+        private readonly ReportDAL _reportController;
+
+        public List<Report> GetMostPopularFurnitureDuringDates(DateTime startDate, DateTime endDate)
+        {
+            return _reportController.GetMostPopularFurnitureDuringDates(startDate, endDate);
+        }
+    }
+}
